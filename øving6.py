ddeaa96a78366ae35d4e4db5897849cd4e708e1c
petterwt <tverdalp@outlook.com>
2024-10-04 09:58:43 +0200

    Skrev ned eksempel på hvordan vi kan dele ut oppgavene

--- øving6.py
+++ øving6.py
@@ -1,8 +1,7 @@
-#tester terminal
-n = 1
-print(n*n)
+# En person kan finne ut hvordan vi skal lese inn fila og legge dataene inn i lister
 
-# tester input
-i = int(input("Skriv et tall: "))
-sum = n + i
-print(f"{n} + {i} = {sum}")
+# En person bør finne ut hvordan vi skal gjøre om feks "11.06.2021" om til en brukbar x-verdi. For eksempel x antall timer eller minutter etter start (første måling). Kun dag, timer og minutter er nødvendig. Filene har ulike formater for dato og tidspunkt, og vi må ha alt i samme format for å plotte data i samme koordinatsystem.
+
+# Noen må skrive koden for hvordan vi skal plotte de ferdige lisene med x- og y verdier, for begge filene. Her er det bare å late som om du har en ferdig liste og plotte den for å teste, siden vi jobber samtidig med forskjellige deler av koden.
+
+# Noen må lage en funksjon som tar inn en liste med tider, ei liste med temperaturer og et tall n som sier hvor mange verdier den skal regne ut gjennomsnittet for. (se g) i oppgaveteksten.)
